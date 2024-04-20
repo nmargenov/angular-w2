e0aa94d96130c369472a3b52429e05f69bc12f79
nmargenov <nikolay_margenov@yahoo.com>
2024-04-20 22:58:17 +0300

    Visualize book

---
+++ src/app/IBook.ts
@@ -0,0 +1,7 @@
+export interface IBook{
+    id:Number,
+    title:String,
+    description:String,
+    author:String,
+    rating:number[],
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IBook } from './IBook';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'booksApp';
+
+  public index = 1;
+
+  public books: IBook[] = [
+    {
+      id: 1,
+      title: "Harry Potter and the Philosopher's Stone",
+      description: "The first book in the Harry Potter series, where Harry discovers he's a wizard and begins his journey at Hogwarts School of Witchcraft and Wizardry.",
+      author: "J.K. Rowling",
+      rating: [4, 5, 4, 5, 3, 5]
+    },
+    {
+      id: 2,
+      title: "The Lord of the Rings",
+      description: "A fantasy epic that follows the quest to destroy the One Ring, which holds great power and threatens Middle-earth.",
+      author: "J.R.R. Tolkien",
+      rating: [5, 4, 5, 4, 5, 4]
+    },
+    {
+      id: 3,
+      title: "To Kill a Mockingbird",
+      description: "A novel by Harper Lee set in the Depression-era South, addressing racial injustice through the trial of a black man accused of raping a white woman.",
+      author: "Harper Lee",
+      rating: [4, 5, 4, 3, 5, 4]
+    },
+    {
+      id: 4,
+      title: "The Catcher in the Rye",
+      description: "A novel by J.D. Salinger, narrated by a teenager who feels disconnected from the adult world and struggles with the phoniness of society.",
+      author: "J.D. Salinger",
+      rating: [3, 4, 3, 4, 3, 4]
+    },
+    {
+      id: 5,
+      title: "Pride and Prejudice",
+      description: "A classic romance novel by Jane Austen, following the lives and love interests of the Bennet sisters in Georgian England.",
+      author: "Jane Austen",
+      rating: [4, 5, 4, 3, 5, 4]
+    },
+    {
+      id: 6,
+      title: "The Great Gatsby",
+      description: "A novel by F. Scott Fitzgerald set in the Jazz Age, portraying the glamorous and decadent lifestyles of the wealthy elite.",
+      author: "F. Scott Fitzgerald",
+      rating: [3, 4, 3, 2, 4, 3]
+    },
+    {
+      id: 7,
+      title: "The Catcher in the Rye",
+      description: "A novel by J.D. Salinger, narrated by a teenager who feels disconnected from the adult world and struggles with the phoniness of society.",
+      author: "J.D. Salinger",
+      rating: [4, 3, 5, 4, 3, 4]
+    }
+  ];
+
+  public calculateRating(ratings:number[]){
+    let sum = 0;
+    ratings.forEach(rate=>{
+      sum+=rate;
+    });
+    const average = sum/ratings.length;
+    return average.toFixed(1);
+  }
 }
